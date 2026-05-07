9c3789ef357a4eedc35f7aa17d09bfa61d2cb3ce
Abhinandan_kore_2880 <abhinandankore2003@gmail.com>
2026-05-07 22:43:07 +0530

    added tree question

---
+++ Day4/findWay.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int ans = 0;
+
+int dfs(int node, vector<pair<int,int>> &tree)
+{
+    if(node == -1)
+        return 0;
+
+    int leftHeight = dfs(tree[node].first, tree);
+    int rightHeight = dfs(tree[node].second, tree);
+
+    ans = max(ans, leftHeight + rightHeight);
+    return 1 + max(leftHeight, rightHeight);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<pair<int,int>> tree(n + 1);
+
+    for(int i = 1; i <= n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        tree[i] = {l, r};
+    }
+    dfs(1, tree);
+
+    cout << ans << endl;
+
+    return 0;
+}
